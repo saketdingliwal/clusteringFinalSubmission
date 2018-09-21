f7e82e9b8804f9103605351f022fe2193f6421c9
saketdingliwal <saketdingliwal97@gmail.com>
2018-09-21 17:25:47 +0530

    reduced size of repo

---
+++ src/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+g++ -O3 -std=c++11 kmeans.cpp -o kmeans
+g++ -O3 -std=c++11 dbscan_2.cpp -o dbscan
+g++ -O3 -std=c++11 optics_2.cpp -o optics
---
+++ src/dbscan.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#include "kdtree.h"
+
+
+using namespace std;
+using namespace kdt;
+
+
+class Point : public std::array<double, 5>
+{
+public:
+	static const int DIM = 5;
+	Point () {}
+	Point(double x, double y, double z, double a, double b)
+	{ 
+		(*this)[0] = x;
+		(*this)[1] = y;
+		(*this)[2] = z;
+		(*this)[3] = a;
+		(*this)[4] = b;
+	}
+};
+
+vector<Point> points;
+vector<int> label;
+
+int main(int argc, char **argv){
+	ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);   
+	int min_points = stoi(argv[1]);
+	double eps = stof(argv[2]);
+  	string file_name = argv[3];
+  	ifstream f(file_name);
+	int act_dim;
+  	int n = 0;
+  	string line;
+  	while(getline(f, line)) {
+    	stringstream stream(line);
+    	vector<double> pt;
+    	while(1) {
+      		double pt1;
+      		stream >> pt1;
+      		pt.push_back(pt1);
+      		if(!stream)
+        		break;
+    	}
+    	act_dim = pt.size();
+    	Point p;
+    	for(int i=0;i<5;i++){
+    		p[i] = 0.0;
+    	}
+    	for(int i=0;i<act_dim;i++){
+    		p[i] = pt[i];
+    	}
+    	points.push_back(p);
+    	label.push_back(-1);
+	    n+=1;
+  	}
+	KDTree<Point> kdtree(points,act_dim);
+	int cluster_count =0;
+	for(int i=0;i<n;i++){
+		if(label[i]!=-1)
+			continue;
+		const vector<int> radIndices = kdtree.radiusSearch(points[i], eps, act_dim);
+		if(radIndices.size()<min_points){
+			label[i] = -2;
+			continue;
+		}
+		cout << "#" << cluster_count <<"\n";
+		cout << i << "\n";
+		label[i] = cluster_count;
+		set <int> seed;
+		vector<int> elem;
+		for(int j=0;j<radIndices.size();j++){
+			if(label[radIndices[j]]<0)
+				elem.push_back(radIndices[j]);
+		}
+		seed.insert(elem.begin(),elem.end());
+		while(seed.size()!=0){
+			int q = *seed.begin();
+			seed.erase(q);
+			if(label[q]==-2){
+				cout << q <<"\n";
+				label[q] = cluster_count;
+				continue;
+			}
+			if(label[q]!=-1)
+				continue;
+			cout << q <<"\n";
+			label[q] = cluster_count;
+			const vector<int> radIndices2 = kdtree.radiusSearch(points[q], eps, act_dim);
+			if(radIndices2.size() >= min_points){
+				vector <int> elem2;
+				for(int j=0;j<radIndices2.size();j++){
+					if(label[radIndices2[j]]<0)
+						elem2.push_back(radIndices2[j]);
+				}
+				seed.insert(elem2.begin(),elem2.end());
+			}
+		}
+		cluster_count +=1;
+	}
+	int outlier = 0;
+	for(int i=0;i<n;i++){
+		if(label[i]==-2)
+			outlier++;
+	}
+	if(outlier==0)
+		return 0;
+	cout << "#outlier\n";
+	for(int i=0;i<n;i++){
+		if(label[i]==-2)
+			cout << i <<"\n";
+	}
+	return 0;
+}
+
